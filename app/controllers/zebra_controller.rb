c538b5f2ab8720c785b39c66c407ebe08eb0c300
Brian Song <brian.cs.song@gmail.com>
2024-10-30 07:11:06 +0000

    Changes to simplify it

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -4,25 +4,25 @@ class ZebraController < ApplicationController
     render({ :template => "dice_templates/rules" })
   end
 
-  def twobysix
-    roll_dice(2, 6)
-    render({ :template => "dice_templates/twobysix" })
-  end
+  # def twobysix
+  #   roll_dice(2, 6)
+  #   render({ :template => "dice_templates/twobysix" })
+  # end
 
-  def twobyten
-    roll_dice(2, 10)
-    render({ :template => "dice_templates/twobyten" })
-  end
+  # def twobyten
+  #   roll_dice(2, 10)
+  #   render({ :template => "dice_templates/twobyten" })
+  # end
 
-  def onebytwenty
-    roll_dice(1, 20)
-    render({ :template => "dice_templates/onebytwenty" })
-  end
+  # def onebytwenty
+  #   roll_dice(1, 20)
+  #   render({ :template => "dice_templates/onebytwenty" })
+  # end
 
-  def fivebyfour
-    roll_dice(5, 4)
-    render({ :template => "dice_templates/fivebyfour" })
-  end
+  # def fivebyfour
+  #   roll_dice(5, 4)
+  #   render({ :template => "dice_templates/fivebyfour" })
+  # end
 
   def roll_dice
     @num_dice = params[:num_dice].to_i
@@ -36,13 +36,4 @@ class ZebraController < ApplicationController
     render({ :template => "dice_templates/roll_result" })
   end
 
-  # def roll_dice(num_dice, sides)
-  #   @num_dice = num_dice
-  #   @sides = sides
-  #   @rolls = []
-
-  #   @num_dice.times do
-  #     die = rand(1..@sides)
-  #     @rolls.push(die)
-  # end
 end
