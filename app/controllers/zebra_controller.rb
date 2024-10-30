4d60e197c6f12c69057b06fdaa473240513b42b9
Brian Song <brian.cs.song@gmail.com>
2024-10-30 06:50:21 +0000

    Further changes

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,38 @@
+class ZebraController < ApplicationController
+  def rules
+    # This action will render the home page
+    render({ :template => "game_templates/rules" })
+  end
+
+  def twobysix
+    roll_dice(2, 6)
+    render({ :template => "dice_templates/twobysix" })
+  end
+
+  def twobyten
+    roll_dice(2, 10)
+    render({ :template => "dice_templates/twobyten" })
+  end
+
+  def onebytwenty
+    roll_dice(1, 20)
+    render({ :template => "dice_templates/onebytwenty" })
+  end
+
+  def fivebyfour
+    roll_dice(5, 4)
+    render({ :template => "dice_templates/fivebyfour" })
+  end
+
+  def roll_dice(num_dice, sides)
+    @num_dice = num_dice
+    @sides = sides
+    @rolls = []
+
+    @num_dice.times do
+      die = rand(1..@sides)
+      @rolls.push(die)
+  end
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "zebra", :action => "rules" })
   
-  get("/2d6", { :controller => "zebra", :action => "2d6" })
+  get("/2d6", { :controller => "zebra", :action => "twobysix" })
 
-  get("/2d10", { :controller => "zebra", :action => "2d10" })
+  get("/2d10", { :controller => "zebra", :action => "twobyten" })
 
-  get("/1d20", { :controller => "zebra", :action => "1d20" })
+  get("/1d20", { :controller => "zebra", :action => "onebytwenty" })
 
-  get("/5d4", { :controller => "zebra", :action => "5d4" })
+  get("/5d4", { :controller => "zebra", :action => "fivebyfour" })
 
 end
