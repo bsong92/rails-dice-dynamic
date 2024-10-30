4428746a76a70f04173df0693bf283927dee6199
Brian Song <brian.cs.song@gmail.com>
2024-10-30 06:37:53 +0000

    Ongoing change

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "zebra", :action => "rules" })
+  
+  get("/2d6", { :controller => "zebra", :action => "2d6" })
+
+  get("/2d10", { :controller => "zebra", :action => "2d10" })
+
+  get("/1d20", { :controller => "zebra", :action => "1d20" })
+
+  get("/5d4", { :controller => "zebra", :action => "5d4" })
+
 end
